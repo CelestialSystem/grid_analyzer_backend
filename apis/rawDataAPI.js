83db3f8e5e029ec99b270ded31561060bce6a346
Rahulgarg30591 <rgarg@celestialsys.com>
2020-04-16 14:07:13 +0530

    SQLConnection

--- apis/rawDataAPI.js
+++ apis/rawDataAPI.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import data from '../data/dataFileRaw.json';
+import { createConnection } from '../helper/sqlHelper';
 const router = express.Router();
 
 /**
@@ -7,31 +8,170 @@ const router = express.Router();
  * @type {[type]}
  */
 router.get('/getData', (req, res, next) => {
-    const targetSize = parseInt(req.query.size, 10);
-    let result = [];
-    
-    if (targetSize <= 1000000) {
-      if (targetSize > 5000) {
-        const multiplier = targetSize/5000;
-        
-        for(let i = 1; i <= multiplier; i++) {
-          result = result.concat(JSON.parse(JSON.stringify(data.users)));
-        }
-        
-        for (let j = 1; j < targetSize; j++) {
-          result[j].id = j;
-        }
-      } else {
-        result = JSON.parse(JSON.stringify(data.users)).filter((user) => {
-          return user.id >= 0 && user.id < targetSize
-        });
-      }
-      
-      res.send(result);
+  const targetSize = parseInt(req.query.size, 10);
+  const filter = parseInt(req.query.filter, 10);
+
+  let result = [];
+  const connection = createConnection();
+
+  connection.connect((err) => {
+    if(err) {
+        res.send(err);
     } else {
-      res.send({ error: 'Max 1 million users can be fetched.' });
+      connection.query(`SELECT * FROM celestial_data where id <= ${targetSize}`, ( err, rows, b ) => {
+        
+        res.send({ users: rows});
+      });  
     }
-      
+    connection.end();
+  });
+});
+
+router.post('/getData', (req, res, next) => {
+  const targetSize = parseInt(req.query.size, 10);
+  const filter = parseInt(req.query.filter, 10);
+
+  let result = [];
+  const connection = createConnection();
+
+  connection.connect((err) => {
+    if(err) {
+        res.send(err);
+    } else {
+      connection.query(`SELECT * FROM celestial_data where id <= ${targetSize}`, ( err, rows, b ) => {
+        res.send({ result: rows, count: 2000 });
+      });  
+    }
+    connection.end();
+  });
+});
+
+/**
+ * Generates and Sends the raw Data over the client on the basis of direct size.
+ * @type {[type]}
+ */
+router.get('/getFilterDataExtClassic', (req, res, next) => {
+  const targetSize = parseInt(req.query.size, 10);
+  const filter = JSON.parse(req.query.filter)[0];
+
+  const connection = createConnection();
+
+  connection.connect((err) => {
+    if(err) {
+        res.send(err);
+    } else {
+      let query = '';
+
+      if (filter) {
+        console.log(filter);
+        const field = filter.property;
+        const value = filter.value;
+
+
+        query = `SELECT * FROM celestial_data where ${field} like "${value}%"`;
+      } else {
+        query = `SELECT * FROM celestial_data where id <= ${targetSize}`;
+      }
+
+      console.log(query);
+      connection.query(`SELECT * FROM celestial_data where id <= ${targetSize}`, ( err, rows, b ) => {
+        res.send(rows);
+      });  
+    }
+    connection.end();
+  });
+});
+
+/**
+ * Generates and Sends the raw Data over the client on the basis of direct size.
+ * @type {[type]}
+ */
+router.get('/getDevExtremeData', (req, res, next) => {
+  const targetSize = parseInt(req.query.size, 10);
+  console.log(req.query);
+  const filter = JSON.parse(req.query.filter);
+  let result = [];
+  const connection = createConnection();
+
+  connection.connect((err) => {
+      if(err) {
+          res.send(err);
+      } else {
+        let query = '';
+
+        if (filter) {
+          const field = filter[0];
+          const value = filter[2];
+          const type = filter[1];
+          console.log(type);
+          console.log(field);
+          console.log(value);
+
+          if (type === '=') {
+            query = `SELECT * FROM celestial_data where id <= ${targetSize} AND ${field}="${value}"`;
+          } else if (type === 'contains') {
+            
+
+            query = `SELECT * FROM celestial_data where id <= ${targetSize} AND ${field} like "${value}%"`;
+          }
+        } else {
+          query = `SELECT * FROM celestial_data where id <= ${targetSize}`;
+        }
+
+        console.log('I AM QUERY');
+        console.log(query);
+        connection.query(query, ( err, rows, b ) => {
+          res.send(rows);
+        });  
+      }
+      // connection.end();
+  });
+});
+
+/**
+ * Generates and Sends the raw Data over the client on the basis of direct size.
+ * @type {[type]}
+ */
+router.get('/getKendoUIData', (req, res, next) => {
+  const targetSize = parseInt(req.query.size, 10);
+  console.log(req.query);
+  const filter = JSON.parse(req.query.filter);
+  let result = [];
+  const connection = createConnection();
+
+  connection.connect((err) => {
+      if(err) {
+          res.send(err);
+      } else {
+        let query = '';
+
+        if (filter) {
+          const field = filter[0];
+          const value = filter[2];
+          const type = filter[1];
+          console.log(type);
+          console.log(field);
+          console.log(value);
+
+          if (type === 'equal') {
+            query = `SELECT * FROM celestial_data where id <= ${targetSize} AND ${field}="${value}"`;
+          } else if (type === 'contains') {
+            
+
+            query = `SELECT * FROM celestial_data where id <= ${targetSize} AND ${field} like "${value}%"`;
+          }
+        } else {
+          query = `SELECT * FROM celestial_data where id <= ${targetSize}`;
+        }
+
+        console.log('I AM QUERY');
+        console.log(query);
+        connection.query(query, ( err, rows, b ) => {
+          res.send(rows);
+        });  
+      }
+      // connection.end();
+  });
 });
 
 /**
@@ -39,32 +179,30 @@ router.get('/getData', (req, res, next) => {
  * @type {[type]}
  */
 router.get('/getPageData', (req, res, next) => {
-    let {
-        start,
-        limit
-    } = req.query;
-    
-    start = parseInt(start, 10);
-    limit = parseInt(limit, 10);
-    
-    
-    if (limit > 5000) {
-      res.send({
-        error: 'Max Limit is 5000'
-      })
+  let {
+      start,
+      limit
+  } = req.query;
+  
+  start = parseInt(start, 10);
+  limit = parseInt(limit, 10);
+  
+  if (limit > 5000) {
+    res.send({
+      error: 'Max Limit is 5000'
+    })
+  }
+
+  connection.connect((err) => {
+    if(err) {
+        res.send(err);
+    } else {
+      connection.query(`SELECT * FROM celestial_data where id => ${start} && id < ${start + limit}`, ( err, rows, b ) => {
+        res.send(rows);
+      });  
     }
-    
-    const newStart = start % 5000;
-    
-    const result = JSON.parse(JSON.stringify(data.users)).filter((user) => {
-      return user.id >= newStart && user.id < newStart + limit
-    });
-    
-    for (let j = 0; j < limit; j++) {
-      result[j].id = start + j;
-    }
-    
-    res.send(result);
+    connection.end();
+  });
 });
 
 export default router;
--- config.js
+++ config.js
@@ -1,3 +1,13 @@
 const targetPort = 4545;
+const username = 'perf-test-app';
+const password = 'ZG9sxX^Cx7ugNCj+';
+const database = 'performance_testing';
+const host = 'db-main-live.sencha.com';
 
-export default targetPort;
+export { 
+    targetPort,
+    username,
+    password,
+    database,
+    host
+};
